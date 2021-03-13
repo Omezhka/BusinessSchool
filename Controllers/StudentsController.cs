7cddcf2dfe77c7fc5ec72da4b175b91feaf137a0
Наталья Аверкова <falabellla-61@mail.ru>
2021-03-13 12:11:58 +0300

    Добавьте файлы проекта.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessSchool.Data;
+using BusinessSchool.Models;
+
+namespace BusinessSchool.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParam"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["CurrentFilter"] = searchString;
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            var students = from s in _context.Students
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.Include(s => s.Enrollments)
+                .ThenInclude(e => e.Course).AsNoTracking().FirstOrDefaultAsync(m => m.ID == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LastName,FirstName,EnrollmentDate")] Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists" + 
+                    "see your system administrator.");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstName,EnrollmentDate")] Student student)
+        {
+            if (id != student.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Data/DbInitializer.cs
@@ -0,0 +1,77 @@
+﻿using BusinessSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessSchool.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+            //просмотр данных в табл student
+            if (context.Students.Any())
+            {
+                return; //заполнена
+            }
+
+            var students = new Student[]
+            {
+                new Student{FirstName="Иван Иванович", LastName="Иванов", EnrollmentDate=DateTime.Parse("2020-09-01")},
+                new Student{FirstName="Николай Алексеевич", LastName="Амосов", EnrollmentDate=DateTime.Parse("2018-09-01")},
+                new Student{FirstName="Артур", LastName="Никифоров", EnrollmentDate=DateTime.Parse("2020-09-01")},
+                new Student{FirstName="Ника", LastName="Александрова", EnrollmentDate=DateTime.Parse("2019-09-01")},
+                new Student{FirstName="Ян Андерс", LastName="Ли", EnrollmentDate=DateTime.Parse("2019-09-01")},
+                new Student{FirstName="Максим Максимович", LastName="Жуков", EnrollmentDate=DateTime.Parse("2018-09-01")},
+                new Student{FirstName="Елена Викторовна", LastName="Морозова", EnrollmentDate=DateTime.Parse("2020-09-01")},
+                new Student{FirstName="Аркадий Алексеевич", LastName="Слован", EnrollmentDate=DateTime.Parse("2019-09-01")}
+            };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+
+            var courses = new Course[]
+            {
+                new Course{CourseID = 1050, Title = "VR и дизайн персонажей", Credits = 3},
+                new Course{CourseID = 4022, Title = "Микроэкономика", Credits = 3},
+                new Course{CourseID = 4041, Title = "Макроэкономика и международные отношения", Credits = 3},
+                new Course{CourseID = 1045, Title = "Защита корпоративных систем", Credits = 4},
+                new Course{CourseID = 3141, Title = "Анализ данных", Credits = 4},
+                new Course{CourseID = 2021, Title = "Глубокое обучение и нейросети", Credits = 3},
+                new Course{CourseID = 2042, Title = "Прикладные информационные системы", Credits = 4}
+            };
+            foreach (Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+            context.SaveChanges();
+
+            var enrollments = new Enrollment[]
+            {
+                new Enrollment{StudentID = 1, CourseID = 1050, Grade = Grade.A},
+                new Enrollment{StudentID = 1, CourseID = 4022, Grade = Grade.C},
+                new Enrollment{StudentID = 1, CourseID = 4041, Grade = Grade.B},
+                new Enrollment{StudentID = 2, CourseID = 1045, Grade = Grade.B},
+                new Enrollment{StudentID = 2, CourseID = 3141, Grade = Grade.F},
+                new Enrollment{StudentID = 2, CourseID = 2021, Grade = Grade.F},
+                new Enrollment{StudentID = 3, CourseID = 1050},
+                new Enrollment{StudentID = 4, CourseID = 1050},
+                new Enrollment{StudentID = 4, CourseID = 4022, Grade = Grade.F},
+                new Enrollment{StudentID = 5, CourseID = 4041, Grade = Grade.C},
+                new Enrollment{StudentID = 6, CourseID = 1045},
+                new Enrollment{StudentID = 7, CourseID = 3141, Grade = Grade.A},
+            };
+
+            foreach (Enrollment e in enrollments)
+            {
+                context.Enrollments.Add(e);
+            }
+            context.SaveChanges();
+        }
+        
+    }
+}
